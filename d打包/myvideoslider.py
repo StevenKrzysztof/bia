36103693ff32dfa132c0ee153150422907640133
StevenKrzysztof <48268079+StevenKrzysztof@users.noreply.github.com>
2021-12-10 18:50:29 +0800

    Add files via upload

---
+++ d打包/myvideoslider.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  6 17:25:11 2021
+
+@author: Ziyang_Wang
+"""
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import QSlider
+
+class myVideoSlider(QSlider):
+    ClickedValue = pyqtSignal(int)
+
+    def __init__(self, father):
+        super().__init__(Qt.Horizontal, father)
+
+    def mousePressEvent(self, QMouseEvent):     #单击事件
+        super().mousePressEvent(QMouseEvent)
+        value = QMouseEvent.localPos().x()
+        # self.setValue(int(value)/9)
+        value = round(value/self.width()*self.maximum())  # 根据鼠标点击的位置和slider的长度算出百分比
+        self.ClickedValue.emit(value)
